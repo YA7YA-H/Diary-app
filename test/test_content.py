7cd04870a06ca669e263a84493a5b62165c8a06a
Hassan Yahya Hussein <husain.host@gmail.com>
2018-07-16 09:35:08 +0300

    Fix typo

--- test/test_content.py
+++ test/test_content.py
@@ -13,11 +13,7 @@ class EntryTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client()
-        self.data = {
-            "ContentID": 1,
-            "Date": "01/01/18",
-            "Content": "I had fun at the zoo"
-        }
+        self.data = {"Date": "01/01/18", "Content": "I had fun at the zoo"}
 
     def test_api_can_get_all_entries(self):
         """Test API can GET all entries """
@@ -29,7 +25,7 @@ class EntryTestCase(unittest.TestCase):
         response = self.client.get('api/v1/user/entries')
         self.assertEqual(response.status_code, 200)
 
-    def test_400_post_a_request(self):
+    def test_400_post_entries(self):
         """Test bad request on post method"""
         empty = self.client.post(
             'api/v1/user/entries', data={}, content_type="application/json")
@@ -42,17 +38,3 @@ class EntryTestCase(unittest.TestCase):
             data=json.dumps(self.data),
             content_type="application/json")
         self.assertEqual(response.status_code, 201)
-
-    def test_api_get_single_entry(self):
-        """Test API url [GET] api/user/{id}"""
-        response = self.client.post(
-            "api/v1/user/entries",
-            data=json.dumps(self.data),
-            content_type="application/json")
-        self.assertEqual(response.status_code, 201)
-        response1 = self.client.get('api/v1/user/entries/1')
-        self.assertEqual(response1.status_code, 200)
-
-    def test_404_get_single_entry(self):
-        response = self.client.get("/api/user/entry/3")
-        self.assertEqual(response.status_code, 404)
